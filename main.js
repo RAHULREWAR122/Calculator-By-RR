05d5d522cd008b513d6881c289024f4ec63d203a
RAHUL REWAR <133180470+RAHULREWAR122@users.noreply.github.com>
2023-08-11 09:58:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+let input = document.getElementById('inp');
+
+let button = document.querySelectorAll('button');
+
+let num = "";
+
+let arr = Array.from(button);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            num = eval(num);
+            input.value = num;
+        }
+        else if (e.target.innerHTML == 'AC') {
+            num = "";
+            input.value = num;
+        }
+        else if (e.target.innerHTML == 'DEL') {
+            num = num.substring(0, num.length - 1);
+            input.value = num;
+        }
+        else {
+            num += e.target.innerHTML;
+            input.value = num;
+        }
+    })
+})
